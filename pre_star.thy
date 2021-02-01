0bdeb3b58142e8d054d4e80e862209a3589d05e9
andsch <andsch@cs.aau.dk>
2021-02-01 17:44:48 +0100

    new definitions and proofs

--- pre_star.thy
+++ pre_star.thy
@@ -7,6 +7,19 @@ begin
 definition pre_star_step :: "transition set \<Rightarrow> transition \<Rightarrow> bool" where
   "pre_star_step rel \<equiv> \<lambda>(p,\<gamma>,q). (\<exists>p' w. (p,\<gamma>) \<hookrightarrow> (p',w) \<and> (p', op_labels w, q) \<in> (transition_star rel))"
 
+inductive pre_star_step' :: "transition set \<Rightarrow> transition set \<Rightarrow> bool" where
+  add_trans: "(p,\<gamma>) \<hookrightarrow> (p',w) \<Longrightarrow> (p', op_labels w, q) \<in> transition_star rel \<Longrightarrow> pre_star_step' rel (rel \<union> {(p,\<gamma>,q)})"
+
+definition pre_star' :: "transition set \<Rightarrow> transition set \<Rightarrow> bool" where
+  "pre_star' \<equiv> rtranclp pre_star_step'"
+
+definition saturated' :: "transition set \<Rightarrow> bool" where
+  "saturated' A \<equiv> \<nexists>B. pre_star_step' A B"
+
+definition pre_star_lim' :: "transition set \<Rightarrow> transition set \<Rightarrow> bool" where
+  "pre_star_lim' A B \<equiv> pre_star' A B \<and> saturated' B"
+
+
 (* Use this rule in the saturation procedure. *)
 definition pre_star_rel where
   "pre_star_rel = saturate pre_star_step"
@@ -57,6 +70,100 @@ next
     done
 qed
 
+
+lemma pre_star_step'_incr: "pre_star_step' A B \<Longrightarrow> A \<subseteq> B"
+proof(induction rule: pre_star_step'.inducts)
+  case (add_trans p \<gamma> p' w q rel)
+  then show ?case 
+    by auto
+qed
+
+lemma pre_star'_incr: "pre_star' A B \<Longrightarrow> A \<subseteq> B"
+  unfolding pre_star'_def proof (induction rule: rtranclp_induct)
+  case base
+  then show ?case by auto
+next
+  case (step y z)
+  then show ?case
+    using pre_star_step'_incr by auto
+qed
+
+lemma pre_star'_incr_transition_star:
+  "pre_star' A A' \<Longrightarrow> transition_star A \<subseteq> transition_star A'"
+  using mono_def pre_star'_incr transition_star_mono by blast
+
+lemma pre_star_lim'_incr_transition_star:
+  "pre_star_lim' A A' \<Longrightarrow> transition_star A \<subseteq> transition_star A'"
+  by (simp add: pre_star'_incr_transition_star pre_star_lim'_def)
+
+lemma step_relp_non_empty: "step_relp (p',w) (p'',u) \<Longrightarrow> w \<noteq> []"
+  by (metis case_prod_conv empty_iff step'.simps(1) step_def step_relp_def)
+
+
+lemma lemma_3_1':
+  assumes "(p',w) \<Rightarrow>\<^sup>* (p,v)"
+      and "(p,v) \<in> language A"
+      and "pre_star_lim' A A'"
+    shows "accepts A' (p',w)"
+  using assms unfolding step_starp_def
+proof (induct rule: converse_rtranclp_induct)
+  case base
+  then have "\<exists>q\<in>Q. F q \<and> (p, v, q) \<in> transition_star A'"
+    unfolding language_def accepts_def using pre_star_lim'_incr_transition_star by auto
+  then show ?case
+    unfolding accepts_def by auto
+next
+  case (step p'w p''u)
+  define p' where "p' = fst p'w"
+  define w  where "w = snd p'w"
+  define p'' where "p'' = fst p''u"
+  define u  where "u = snd p''u"
+  have p'w_def: "p'w = (p', w)"
+    using p'_def w_def by auto
+  have p''u_def: "p''u = (p'', u)"
+    using p''_def u_def by auto
+
+  have "accepts A' p''u" 
+    using step by auto
+  then obtain q where q_p: "q \<in> Q \<and> F q \<and> (p'', u, q) \<in> transition_star A'"
+    unfolding accepts_def using p''_def u_def by auto
+  then have "(p'', u, q) \<in> transition_star A'"
+    by auto
+  have "\<exists>\<gamma> w1 u1. w=\<gamma>#w1 \<and> u=op_labels u1@w1 \<and> (p', \<gamma>) \<hookrightarrow> (p'', u1)"
+  proof -
+    from step(1) obtain \<gamma> w1 where w_exp: "w=\<gamma>#w1"
+      unfolding p''u_def step_def p'w_def using step_relp_non_empty using list.exhaust by blast 
+    from step(1) have "\<exists>u1. u=op_labels u1@w1 \<and> (p', \<gamma>) \<hookrightarrow> (p'', u1)" 
+      unfolding step_relp_def p''u_def
+        step_def p'w_def w_exp by auto
+    then show "\<exists>\<gamma> w1 u1. w=\<gamma>#w1 \<and> u=op_labels u1@w1 \<and> (p', \<gamma>) \<hookrightarrow> (p'', u1)"
+      using w_exp by auto
+  qed
+  then obtain \<gamma> w1 u1 where \<gamma>_w1_u1_p: "w=\<gamma>#w1 \<and> u=op_labels u1@w1 \<and> (p', \<gamma>) \<hookrightarrow> (p'', u1)"
+    by blast
+
+  obtain q1 where q1_p: "(p'', op_labels u1, q1) \<in> transition_star A' \<and> (q1, w1, q) \<in> transition_star A'"
+    using \<gamma>_w1_u1_p accepts_unfoldn add_trans p''u_def pre_star_lim'_def saturated'_def step.hyps(3) step.prems(1) step.prems(2) by blast
+
+  then have "(p', \<gamma>, q1) \<in> A'"
+    using \<gamma>_w1_u1_p add_trans pre_star_lim'_def saturated'_def step.prems(2) by blast
+
+  then have "(p', \<gamma>, q1) \<in> A' \<and> (q1, w, q) \<in> transition_star A'"
+    using \<gamma>_w1_u1_p q1_p add_trans pre_star_lim'_def saturated'_def step.prems(2) by blast
+
+  then have "(p', \<gamma>#w1, q) \<in> transition_star A'"
+    using transition_star_step q1_p by blast 
+  then have t_in_A': "(p', w, q) \<in> transition_star A'"
+    using \<gamma>_w1_u1_p by blast
+
+  from q_p t_in_A' have "F q \<and> (p', w, q) \<in> transition_star A'"
+    using p'_def w_def by auto
+  then show ?case
+    unfolding accepts_def p'w_def using q_p by auto 
+qed
+
+
+
 (* Lemma 3.2 on page 34 turned out to be harder to prove. 
    Here goes several failed attempts... *)
 
@@ -94,8 +201,67 @@ lemma pre_star_rel_induct: "\<lbrakk>P rel_0; \<And>rel. P rel \<Longrightarrow>
 lemma "\<lbrakk>(p, w, q) \<in> transition_star (pre_star_saturation rel_0 rel_i); 
         (p, w, q) \<notin> transition_star rel_i\<rbrakk>
          \<Longrightarrow> \<exists>p' w'. (p, w) \<Rightarrow>\<^sup>* (p', w') \<and> (p', w', q) \<in> transition_star rel_0"
-  
   oops
+
+lemma lemma_3_2_b_aux':
+  assumes "\<nexists>q \<gamma> q'. (q, \<gamma>, q') \<in> A \<and> q' \<in> P"
+  assumes "(p, w, q) \<in> transition_star A"
+  assumes "q \<in> P"
+  shows "w = [] \<and> p = q"
+using assms(2) assms(1,3) proof (induction)
+  case (transition_star_refl p)
+  then show ?case by auto
+next
+  case (transition_star_step p \<gamma> q' w q)
+  then show ?case by blast
+qed
+
+fun start_of' :: "transition \<Rightarrow> state" where (* TODO: rename *)
+  "start_of' (q, \<gamma>, q') = q"
+
+fun end_of' :: "transition \<Rightarrow> state" where (* TODO: rename *)
+  "end_of' (q, \<gamma>, q') = q'"
+
+fun sym_of' :: "transition \<Rightarrow> label" where (* TODO: rename *)
+  "sym_of' (q, \<gamma>, q') = \<gamma>"
+
+fun consecs' :: "'a list \<Rightarrow> ('a * 'a) set" where
+  "consecs' [] = {}"
+| "consecs' [y] = {}"
+| "consecs' (x#y#xs) = {(x,y)} \<union> consecs' (y#xs)"
+
+lemma lemma_3_2_a':
+  assumes "\<nexists>q \<gamma> q'. (q, \<gamma>, q') \<in> A \<and> q' \<in> P"
+  assumes "pre_star' A A'"
+  assumes "(p, w, q) \<in> transition_star A'"
+  shows "\<exists>p' w'. (p', w', q) \<in> transition_star A \<and> (p, w) \<Rightarrow>\<^sup>* (p', w')"
+  using assms(2) assms(1,3) unfolding pre_star'_def proof (induction rule: rtranclp_induct)
+  case base
+  then show ?case
+    unfolding step_starp_def by auto
+next
+  case (step Aiminus1 Ai)
+  from step(2) obtain p1 \<gamma> p' w q' where p1_\<gamma>_p'_w_q'_p: "
+                       Ai = Aiminus1 \<union> {(p1, \<gamma>, q')} \<and> 
+                       (p1, \<gamma>) \<hookrightarrow> (p', w) \<and> 
+                       (p', op_labels w, q') \<in> transition_star Aiminus1"
+    unfolding pre_star_step'.simps by auto
+  define t where "t = (p1, \<gamma>, q')"
+  (* I think there is a challenge here.
+     In the proof he looks at << p \<midarrow>w\<rightarrow>*_i q >> as if it were a path. But there can be
+     several paths like that. So I need to fix one.
+
+     Hack: pick the smallest subset of Ai such that p \<midarrow>w\<rightarrow>*_i q \<in> Ai.
+
+  *)
+  
+  
+  
+
+  then show ?case sorry
+qed
+
+  
 
 lemma lemma_3_2:
   assumes "(p, w, q) \<in> transition_star (pre_star_rel rel)"
